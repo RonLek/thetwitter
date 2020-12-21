7efd37009aff455a778c11e0de4760702c284dcf
Rohan Lekhwani <rohanlekhwani@ymail.com>
2020-12-21 15:43:01 +0530

    Changes database from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_03_102358) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
